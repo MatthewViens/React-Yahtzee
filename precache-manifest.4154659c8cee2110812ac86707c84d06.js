3d443648778daff754e891ce11e7ce9ca60dfda7
MatthewViens <matthew.j.viens@gmail.com>
2019-05-30 03:24:59 -0700

    Updates

---
+++ precache-manifest.4154659c8cee2110812ac86707c84d06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6c0291b7f25b688167fa3ae58399688",
+    "url": "/React-Yahtzee/index.html"
+  },
+  {
+    "revision": "b628a492c2e932912c21",
+    "url": "/React-Yahtzee/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "c6b726aa9daeb9d5d0c8",
+    "url": "/React-Yahtzee/static/css/main.7316b5f6.chunk.css"
+  },
+  {
+    "revision": "b628a492c2e932912c21",
+    "url": "/React-Yahtzee/static/js/2.0212eb43.chunk.js"
+  },
+  {
+    "revision": "c6b726aa9daeb9d5d0c8",
+    "url": "/React-Yahtzee/static/js/main.b6e73401.chunk.js"
+  },
+  {
+    "revision": "6526e5172830003728ce",
+    "url": "/React-Yahtzee/static/js/runtime~main.d5ee2dfc.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React-Yahtzee/precache-manifest.4154659c8cee2110812ac86707c84d06.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Yahtzee/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
